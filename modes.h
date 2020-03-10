55b0544d5447833f5b9ce91b61037ff1926b4647
Nidhi Vedantam <nidhived@Nidhis-MacBook-Pro.local>
2020-03-10 13:28:44 -0700

    add .h file

---
+++ modes.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+using namespace std;
+    int cmode(string cellArrayMap[], string CellArrayRandom[], int rowMap, int z);
+    int dmode(string cellArrayMap[], string CellArrayRandom[], int rowMap, int z);
+    int mmode(string cellArrayMap[], string CellArrayRandom[], int rowMap, int z);
