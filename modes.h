336aa420f47928f9ab9bb4f177d6d27b170ded08
Yuki Chen <yingchen@chapman.edu>
2020-03-10 13:39:53 -0700

    add comment

--- modes.h
+++ modes.h
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+//initialize array under mapfile and random assignment 
     int cmode(string cellArrayMap[], string CellArrayRandom[], int rowMap, int z);
     int dmode(string cellArrayMap[], string CellArrayRandom[], int rowMap, int z);
     int mmode(string cellArrayMap[], string CellArrayRandom[], int rowMap, int z);
