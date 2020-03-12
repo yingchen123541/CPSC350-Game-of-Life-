c1925a1b1ad5eafba65c1b11730825897a411dd6
Nidhi Vedantam <nidhived@Nidhis-MacBook-Pro.local>
2020-03-12 16:56:13 -0700

    edits

--- GameOfLifeMain.cpp
+++ GameOfLifeMain.cpp
@@ -395,6 +395,133 @@ for(int i = 0; i < row; ++i){
                {
                  //pause between generations
                  cout << "pause between generations" << endl;
+                 int neighbors = 0;
+                 int Row=rowMap;
+                 int Column= z;
+                 string nextgenArray[Row][Column];
+                 int nextnumberArray[Row][Column];
+                 int numberArray[Row][Column];
+                 // in case row - 1 = -1
+                 int Rplus;
+                 int Rminus;
+                 int Cplus;
+                 int Cminus;
+
+               for(int i = 0; i <= 100; i++){
+                 cout << endl;
+                 cout << "Generation # " << i << endl;
+                 for(int c = 0; c < Row; ++c){
+                   for(int d = 0; d < Column; ++d){
+                     if (cellArrayMap[c][d] == "X"){
+                       numberArray[c][d] = 1;
+                    } else if(cellArrayMap[c][d] == "-"){
+                      numberArray[c][d] = 0;
+                    }
+                   }
+                 }
+
+                 if (Row+1 == row) {
+                     Rplus = row;
+                   } else {
+                     Rplus = Row+1;
+                   }
+                   if (Row-1 == -1) {
+                       Rminus = Row;
+                     } else {
+                       Rminus = Row-1;
+                   }if (Column+1 == column) {
+                       Cplus = column;
+                   } else {
+                       Cplus = Column+1;
+                   }if (Column-1 == -1) {
+                       Cminus = column;
+                     } else {
+                       Cminus = Column-1;
+                     }
+   ///////////////////////////////////////////////
+   // r minus and c
+
+
+                   if(cellArrayMap[Row][Cplus] == "X"){
+                     neighbors++;
+                   }
+                   if(cellArrayMap[Row][Cminus] == "X"){
+                     neighbors++;
+                   }
+                   if(cellArrayMap[Rminus][Cminus] == "X"){
+                     neighbors++;
+                   }
+                   if(cellArrayMap[Rminus][Cplus] == "X"){
+                     neighbors++;
+                   }
+                   if(cellArrayMap[Rminus][Column] == "X"){
+                     neighbors++;
+                   }
+                   if(cellArrayMap[Rplus][Cplus] == "X"){
+                     neighbors++;
+                   }
+                   if(cellArrayMap[Rplus][Cminus] == "X"){
+                     neighbors++;
+                   }
+                   if(cellArrayMap[Rplus][Column] == "X"){
+                     neighbors++;
+                   }
+
+   //////////////////////////////////
+
+                   for(int c = 0; c < Row; ++c){
+                     for(int d = 0; d < Column; ++d){
+                     //  cout << "n: " << neighbors << endl;
+                       if(neighbors < 2){
+                          nextnumberArray[c][d] = 0;
+                          nextgenArray[c][d] = "-";
+                        }//end if
+
+              ////if there's 2 neighbor, next generation cell lives on, if empty, still empty
+                      else if(neighbors == 2){
+                        if(numberArray[c][d] == 1){
+                          nextnumberArray[c][d] = 1;
+                          nextgenArray[c][d] = "X";
+                        }//end if
+                        else if(numberArray[c][d] == 0){
+                          nextnumberArray[c][d] = 0;
+                          nextgenArray[c][d] = "-";
+                        }//end else if
+                      }//end else if
+
+              //if there's 3 neighbor, next generation cell lives on, if empty, generate a new cell
+                      else if(neighbors == 3){
+                        if(numberArray[c][d] == 1){
+                            nextnumberArray[c][d] = 1;
+                            nextgenArray[c][d] = "X";
+                          }//end if
+                        }else if(numberArray[c][d] == 0){
+                            nextnumberArray[c][d] = 1;
+                            nextgenArray[c][d] = "X";
+                          }//end else if
+
+              //if there's more than 4 neighbor,overpopulation, cell die
+                        else if(neighbors >= 4){
+                           nextnumberArray[c][d] = 0;
+                           nextgenArray[c][d] = "-";
+                         }
+         }
+         }
+
+         for(int i = 0; i < row; ++i){
+           for(int j = 0; j < column; ++j){
+             if (nextnumberArray[i][j] == 1){
+               cout << "X";
+             }else{
+               cout << "-";
+             }
+           }
+           cout << endl;
+         }
+
+
+
+                   }
                }
                else if (choice=="no")
                {
@@ -867,7 +994,6 @@ cout << endl;
 }
 
 
-
     }
     else if (mode=="mirror")
     {
@@ -877,6 +1003,130 @@ cout << endl;
      if (choice=="yes")
      {
        cout << "pause between generations" << endl;
+
+            int neighborsrandomMirror = 0;
+            int RowrandomMirror=i;
+            int ColumnrandomMirror=j;
+            string nextgenRandomMirror[RowrandomMirror][ColumnrandomMirror];
+            int nextnumberArrayRandomMirror[RowrandomMirror][ColumnrandomMirror];
+            int numberArrayRandomMirror[RowrandomMirror][ColumnrandomMirror];
+
+            int RplusRandom;
+            int RminusRandom;
+            int CplusRandom;
+            int CminusRandom;
+            //print out 100 generations
+            for(int k = 0; k <= 100; k++){
+              cout << endl;
+              cout << "Generation # " << k << endl;
+              for(int c = 0; c < RowrandomMirror; ++c){
+                for(int d = 0; d < ColumnrandomMirror; ++d){
+                  //convert X and - to 1 and 0
+                  if (CellArrayRandom[c][d] == "X"){
+                    numberArrayRandomMirror[c][d] = 1;
+                 } else if(CellArrayRandom[c][d] == "-"){
+                   numberArrayRandomMirror[c][d] = 0;
+                 }
+                }
+              }
+
+              if (RowrandomMirror+1 == row) {
+                  RplusRandom = row;
+                } else {
+                  RplusRandom = RowrandomMirror+1;
+                }
+                if (RowrandomMirror-1 == -1) {
+                    RminusRandom = row;
+                  } else {
+                    RminusRandom = RowrandomMirror-1;
+                }if (ColumnrandomMirror+1 == column) {
+                    CplusRandom = column;
+                } else {
+                    CplusRandom = ColumnrandomMirror+1;
+                }if (ColumnrandomMirror-1 == -1) {
+                    CminusRandom = column;
+                  } else {
+                    CminusRandom = ColumnrandomMirror-1;
+                  }
+
+
+                  if(CellArrayRandom[RowrandomMirror][CplusRandom] == "X"){
+                    neighborsrandomMirror++;
+                  }
+                  if(CellArrayRandom[RowrandomMirror][CminusRandom] == "X"){
+                    neighborsrandomMirror++;
+                  }
+                  if(CellArrayRandom[RminusRandom][CminusRandom] == "X"){
+                    neighborsrandomMirror++;
+                  }
+                  if(CellArrayRandom[RminusRandom][CplusRandom] == "X"){
+                    neighborsrandomMirror++;
+                  }
+                  if(CellArrayRandom[RminusRandom][CplusRandom] == "X"){
+                    neighborsrandomMirror++;
+                  }
+                  if(CellArrayRandom[RplusRandom][CplusRandom] == "X"){
+                    neighborsrandomMirror++;
+                  }
+                  if(CellArrayRandom[RplusRandom][CminusRandom] == "X"){
+                    neighborsrandomMirror++;
+                  }
+                  if(CellArrayRandom[RplusRandom][CplusRandom] == "X"){
+                    neighborsrandomMirror++;
+                  }
+
+                  for(int c = 0; c < RowrandomMirror; ++c){
+                    for(int d = 0; d < ColumnrandomMirror; ++d){
+                    //  cout << "n: " << neighbors << endl;
+                      if(neighborsrandomMirror < 2){
+                         nextnumberArrayRandomMirror[c][d] = 0;
+                         nextgenRandomMirror[c][d] = "-";
+                       }//end if
+
+
+               ////if there's 2 neighbor, next generation cell lives on, if empty, still empty
+                     else if(neighborsrandomMirror == 2){
+                        if(numberArrayRandomMirror[c][d] == 1){
+                           nextnumberArrayRandomMirror[c][d] = 1;
+                           nextgenRandomMirror[c][d] = "X";
+                       }//end if
+                         else if(numberArrayRandomMirror[c][d] == 0){
+                           nextnumberArrayRandomMirror[c][d] = 0;
+                           nextgenRandomMirror[c][d] = "-";
+                        }//end else if
+                        }//end else if
+
+           //if there's 3 neighbor, next generation cell lives on, if empty, generate a new cell
+                     else if(neighborsrandomMirror == 3){
+                         if(numberArrayRandomMirror[c][d] == 1){
+                             nextnumberArrayRandomMirror[c][d] = 1;
+                             nextgenRandomMirror[c][d] = "X";
+                        }//end if
+                      }else if(numberArrayRandomMirror[c][d] == 0){
+                            nextnumberArrayRandomMirror[c][d] = 1;
+                            nextgenRandomMirror[c][d] = "X";
+                         }//end else if
+
+            //if there's more than 4 neighbor,overpopulation, cell die
+                           else if(neighborsrandomMirror >= 4){
+                             nextnumberArrayRandomMirror[c][d] = 0;
+                             nextgenRandomMirror[c][d] = "-";
+                         }
+                       }
+                       }
+
+                         for(int i = 0; i < row; ++i){
+                           for(int j = 0; j < column; ++j){
+                               if (nextnumberArrayRandomMirror[i][j] == 1){
+                                   cout << "X";
+                             }else{
+                                   cout << "-";
+                                 }
+                               }
+                               cout << endl;
+                             }
+                           }
+
      }
      else if (choice=="no")
      {
