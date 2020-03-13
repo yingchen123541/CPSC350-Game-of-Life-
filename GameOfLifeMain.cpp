bc5848b4c80f46fd731d1b0681df92ffac3a47e3
Nidhi Vedantam <nidhived@Nidhis-MacBook-Pro.local>
2020-03-12 17:15:04 -0700

    hi

--- GameOfLifeMain.cpp
+++ GameOfLifeMain.cpp
@@ -277,6 +277,117 @@ else if (mode=="doughnut")
         if (choice=="yes")
         {
           cout << "pause between generations" << endl;
+          int neighbors = 0;
+          int Row=rowMap;
+          int Column= z;
+          string nextgenArray[Row][Column];
+          int nextnumberArray[Row][Column];
+          int numberArray[Row][Column];
+          // in case row - 1 = -1
+          int protectedr;
+          int protectedc;
+
+        for(int i = 0; i <= 100; i++){
+          cout << endl;
+          cout << "Generation # " << i << endl;
+          for(int c = 0; c < Row; ++c){
+            for(int d = 0; d < Column; ++d){
+              if (cellArrayMap[c][d] == "X"){
+                numberArray[c][d] = 1;
+             } else if(cellArrayMap[c][d] == "-"){
+               numberArray[c][d] = 0;
+             }
+            }
+          }
+
+
+          if (Row-1 == -1) {
+              protectedr = Row-1;
+            } else {
+              protectedr = Row-1;
+            }
+            if (Column-1 == -1) {
+              protectedc = Column-1;
+            } else {
+              protectedc = Column-1;
+            }
+
+            if(cellArrayMap[Row][(Column + 1) % column] == "X"){
+              neighbors++;
+            }
+            if(cellArrayMap[(Row+1)%row][Column] == "X"){
+              neighbors++;
+            }
+            if(cellArrayMap[(Row+1)%row][(Column+ 1) % column] == "X"){
+              neighbors++;
+            }
+            if(cellArrayMap[(Row+1)%row][protectedc] == "X"){
+              neighbors++;
+            }
+            if(cellArrayMap[Row][protectedc] == "X"){
+              neighbors++;
+            }
+            if(cellArrayMap[protectedr][protectedc] == "X"){
+              neighbors++;
+            }
+            if(cellArrayMap[protectedr][(Column+ 1) % column] == "X"){
+              neighbors++;
+            }
+            if(cellArrayMap[protectedr][Column] == "X"){
+              neighbors++;
+            }
+
+
+            for(int c = 0; c < Row; ++c){
+              for(int d = 0; d < Column; ++d){
+              //  cout << "n: " << neighbors << endl;
+                if(neighbors < 2){
+                   nextnumberArray[c][d] = 0;
+                   nextgenArray[c][d] = "-";
+                 }//end if
+
+       ////if there's 2 neighbor, next generation cell lives on, if empty, still empty
+               else if(neighbors == 2){
+                 if(numberArray[c][d] == 1){
+                   nextnumberArray[c][d] = 1;
+                   nextgenArray[c][d] = "X";
+                 }//end if
+                 else if(numberArray[c][d] == 0){
+                   nextnumberArray[c][d] = 0;
+                   nextgenArray[c][d] = "-";
+                 }//end else if
+               }//end else if
+
+       //if there's 3 neighbor, next generation cell lives on, if empty, generate a new cell
+               else if(neighbors == 3){
+                 if(numberArray[c][d] == 1){
+                     nextnumberArray[c][d] = 1;
+                     nextgenArray[c][d] = "X";
+                   }//end if
+                 }else if(numberArray[c][d] == 0){
+                     nextnumberArray[c][d] = 1;
+                     nextgenArray[c][d] = "X";
+                   }//end else if
+
+       //if there's more than 4 neighbor,overpopulation, cell die
+                 else if(neighbors >= 4){
+                    nextnumberArray[c][d] = 0;
+                    nextgenArray[c][d] = "-";
+                  }
+  }
+  }
+
+  for(int i = 0; i < row; ++i){
+    for(int j = 0; j < column; ++j){
+      if (nextnumberArray[i][j] == 1){
+        cout << "X";
+      }else{
+        cout << "-";
+      }
+    }
+    cout << endl;
+  }
+  }
         }
         else if (choice=="no")
         {
@@ -1138,10 +1249,6 @@ OutputFile << endl;
 }
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8b79f519cb1af421bd6e4d1f60ccc26692d15e0f
 }
 
     }
