c2fffcc26bd339d538b4769ad9861f432da988e5
yingchen123541 <54613135+yingchen123541@users.noreply.github.com>
2020-03-12 20:23:24 -0700

    Delete modes.h

--- modes.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-using namespace std;
-//initialize array under mapfile and random assignment 
-    int cmode(string cellArrayMap[], string CellArrayRandom[], int rowMap, int z);
-    int dmode(string cellArrayMap[], string CellArrayRandom[], int rowMap, int z);
-    int mmode(string cellArrayMap[], string CellArrayRandom[], int rowMap, int z);
